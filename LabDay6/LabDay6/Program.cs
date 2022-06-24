7fe6db72f2b158d508a86493fc2d8ca7c21a60aa
revanzli <revanzekeryeli2002@gmail.com>
2022-06-24 19:37:15 +0400

    initial commit

---
+++ LabDay6/LabDay6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LabDay6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
