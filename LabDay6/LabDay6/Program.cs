53f3a6abc459a500dca0a8c64fa7423388fc4b4f
revanzli <revanzekeryeli2002@gmail.com>
2022-06-24 19:47:08 +0400

    added task 1 / CustomSubString

--- LabDay6/LabDay6/Program.cs
+++ LabDay6/LabDay6/Program.cs
@@ -6,7 +6,29 @@ namespace LabDay6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(CustomSubString("revan",1,1));
+        }
+        public static string CustomSubString(string text, int startIndex)
+        {
+            string newText = "";
+            for (int i = startIndex; i < text.Length; i++)
+            {
+                newText += text[i];
+            }
+            return newText;
+        }
+        public static string CustomSubString(string text, int startIndex, int lenght)
+        {
+            string newText = "";
+            if(text.Length < startIndex + lenght)
+            {
+                return "Sehvdi";
+            }
+            for (int i = startIndex; i < lenght + startIndex; i++)
+            {
+                newText += text[i];
+            }
+            return newText;
         }
     }
 }
